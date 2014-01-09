cc803203a7e3b1d9f85b21c80cf1dca4db23d730
Albertowska <betomartinkio@gmail.com>
2014-01-09 22:13:57 +0100

    Control en el servidor

--- proyecto/changeMonitorServer/src/main/java/server/DatosDAO.java
+++ proyecto/changeMonitorServer/src/main/java/server/DatosDAO.java
@@ -1,5 +1,10 @@
 package server;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import monitor.MonitoringThread;
 
 import com.google.gson.Gson;
@@ -13,22 +18,57 @@ public class DatosDAO {
 			//Recibe los datos y los transforma en un objetoForm
 			form = gson.fromJson(json, Form.class);
 			
-			//Obtiene la urlReal de la base de datos
-			Facade f = new Facade();
-			String realUrl = f.getRealURL(form.getShortUrl());
-			CompleteForm CForm = new CompleteForm(form, realUrl);
+			String error = validateForm(form);
 			
-			//Start thread 
-			MonitoringThread m = new  MonitoringThread(CForm);
-			Thread r = new Thread(m); 
-			r.start();
-			
-			//Envio del Mail inicial
-			return "Peticion de monitorizacion para la pagina "+realUrl+" recibida.";
+			if(error.equals("")){
+				//Obtiene la urlReal de la base de datos
+				Facade f = new Facade();
+				String realUrl = f.getRealURL(form.getShortUrl());
+				if(realUrl==null){
+					return "La url "+form.getShortUrl()+" no se encuentra en la base de datos.";
+				}else{
+					CompleteForm CForm = new CompleteForm(form, realUrl);
+					
+					//Start thread 
+					MonitoringThread m = new  MonitoringThread(CForm);
+					Thread r = new Thread(m); 
+					r.start();
+					
+					//Envio del Mail inicial
+					return "Peticion de monitorizacion para la pagina "+realUrl+" recibida.";
+				}
+			}
+			return error;
 		}catch(Exception ex){
 			ex.printStackTrace();
 			return ex.getMessage();
 		}		
+	}
+	
+	public String validateForm(Form form){
+		String result = "";
+		if(form.getShortUrl().contains("'")){
+			result = result + "URL contiene caracteres no válidos ";
+		}
+		if(form.getFreq()<5 || form.getFreq()>300){
+			result = result + "Frecuencia fuera de rango ";
+		}
+		if(form.getZone()<-12 || form.getZone()>12){
+			result = result + "Zona fuera de rango ";
+		}
+		long hour = 3600000;
+		if(form.getFechaFin().before(new Timestamp(new Date().getTime()-form.getZone()*hour))){
+			result = result + "Fecha final incorrecta ";
+		}
+		String EMAIL_PATTERN = 
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+				+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcher = pattern.matcher(form.getEmail());
+		if(!matcher.matches()){
+			result = result + "Email incorrecto";
+		}
+		return result;
 	}
 	
 }
